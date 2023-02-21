c1091afef1a46a134831ce39055da5300ab853b2
Fv197 <asoraa@gamil.com>
2023-02-21 19:10:25 +0100

    edit filname

---
+++ post-install/00-snapper.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "*** Installing snapper, grub-btrfs and snap-pac ***"
+pacman -S --noconfirm snapper grub-btrfs snap-pac
+
+echo "*** Configure snapper ***"
+umount /.snapshots
+rm -r /.snapshots
+snapper -c root create-config /
+btrfs subvolume delete /.snapshots/
+mkdir /.snapshots
+mount -a
+ID=$(btrfs subvol list / | head -n 1 | awk '{print $2}')
+btrfs subvolume set-default $ID /
+sed -i 's/ALLOW_GROUPS=""/ALLOW_GROUPS="wheel"/' /etc/snapper/configs/root
+sed -i 's/TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="5"/' /etc/snapper/configs/root
+sed -i 's/TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="7"/' /etc/snapper/configs/root
+sed -i 's/TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMiT_MONTHLY="0"/' /etc/snapper/configs/root
+sed -i 's/TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"/' /etc/snapper/configs/root
+chown -R :wheel /.snapshots
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
+
+echo "*** Creating base snapshot ***"
+snapper -c root create -d "*** BASE ***"
