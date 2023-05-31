f380370c69bc7f93ed8ec5958d7cd62acdd2c527
Fv197 <asoraa@gmail.com>
2023-05-31 18:08:31 +0200

    Added .desktop files to remove unwanted icons in application grid

--- post-install/01-gnome-config.sh
+++ post-install/01-gnome-config.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# Script to be run as normal user
+
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type hibernate
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 1800
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type hibernate
-gsettings set org.gnome.settings-daemon.plugins.power power-button-action suspend
 
 sudo echo "MOZ_ENABLE_WAYLAND=1" >> /etc/environment
+
+mkdir $HOME/.local/share/applications
+cp applications/* $HOME/.local/share/applications
 
 # Todo
 # Install and enable alphabetical-grid-extension
